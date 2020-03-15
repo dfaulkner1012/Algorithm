634881baab9d0db29d7b1f2ef89b15d123bb04de
dfaulkner1012 <derek_joseph06@hotmail.com>
2020-03-14 19:46:38 -0700

    updated code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,12 +8,18 @@ public class Main {
             A prime number is a number that's only divisible by 1 and itself. If it's prime, return true.
             Otherwise return false.
         */
+        /*
+            1. Change your for loop to this: (int i = 2; i < primNum; i++) Remember, a prime number is a number
+            that is divisible only by 1 and itself.
+            Therefore, you want to check, if it's divisible by any number between 2 and itself.
+            2. Change your if block based on the above.
+        */
         System.out.println(checkIfPrimeNum(5));
     }
 
     public static boolean checkIfPrimeNum(int primNum) {
-        for (int i = 5; i < 10; i++) {
-            if (i % 1 == 0) {
+        for (int i = 2; i < primNum; i++) {
+            if (i % 2 == 2) {
                 return true;
             }
         }
